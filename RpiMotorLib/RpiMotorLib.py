3619de74a72b4387b94671e9eb7213616eedd6d5
Michael Springsits <michael@springsits.cc>
2022-10-13 21:09:10 +0200

    changes positional arg of ramp_steps

--- RpiMotorLib/RpiMotorLib.py
+++ RpiMotorLib/RpiMotorLib.py
@@ -245,7 +245,7 @@ class A4988Nema(object):
             GPIO.output(self.mode_pins, resolution[steptype])
 
     def motor_go(self, clockwise=False, steptype="Full",
-                 steps=200, stepdelay=.005, ramp_steps = 100, verbose=False, initdelay=.05):
+                 steps=200, stepdelay=.005, verbose=False, initdelay=.05, ramp_steps = 100):
         """ motor_go,  moves stepper motor based on 6 inputs
 
          (1) clockwise, type=bool default=False
